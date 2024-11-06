25b96c6de7dd9e34f1ad379677d2a3b3cb8ebb0b
Taha Zoabi <zoabi03@gmail.com>
2024-11-06 20:39:22 +0200

    migration fix for laptop

--- prisma/migrations/20241106183214_unique/migration.sql
+++ prisma/migrations/20241106183214_unique/migration.sql
@@ -24,5 +24,8 @@ CREATE TABLE "products" (
     CONSTRAINT "products_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
 -- AddForeignKey
 ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
