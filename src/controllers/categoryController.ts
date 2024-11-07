207fc9370f29782691d371ed073ae1a604c44fb6
Taha Zoabi <zoabi03@gmail.com>
2024-11-07 16:36:51 +0200

    http errors

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
 
 const prisma = new PrismaClient();
 
@@ -7,7 +8,7 @@ export const getCategories: RequestHandler = async (req, res, next) => {
   try {
     const categories = await prisma.category.findMany();
     if (!categories) {
-      res.status(404).json({ error: "No Categories were found!" });
+      throw createHttpError(404, "Categories were not found");
     }
     res.status(201).json(categories);
   } catch (e) {
@@ -22,7 +23,7 @@ export const getCategory: RequestHandler = async (req, res, next) => {
     const validID = validateID(id);
 
     if (!validID) {
-      res.status(400).json({ error: "Invalid ID parameter!" });
+      throw createHttpError(400, "Invalid Category ID");
     }
 
     const category = await prisma.category.findUnique({
@@ -31,7 +32,7 @@ export const getCategory: RequestHandler = async (req, res, next) => {
       },
     });
     if (!category) {
-      res.status(404).json({ error: " Category was not found!" });
+      throw createHttpError(404, "Category not found");
     }
     res.status(201).json(category);
   } catch (e) {
@@ -45,7 +46,7 @@ export const createCategory: RequestHandler = async (req, res, next) => {
     const categoryName = req.body.name;
 
     if (!categoryName) {
-      res.status(422).json({ nameError: "Name is required" });
+      throw createHttpError(400, "Category must have a title");
     }
 
     const existingName = await prisma.category.findUnique({
@@ -74,10 +75,10 @@ export const updateCategory: RequestHandler = async (req, res, next) => {
     const newCategoryName = req.body.name;
 
     if (!validID) {
-      res.status(400).json({ error: "Invalid ID parameter!" });
+      throw createHttpError(400, "Invalid Category ID");
     }
     if (!newCategoryName) {
-      res.status(400).json({ error: "Category must have a title!" });
+      throw createHttpError(400, "Category must have a title");
     }
     if (
       !(await prisma.category.findUnique({
@@ -86,7 +87,7 @@ export const updateCategory: RequestHandler = async (req, res, next) => {
         },
       }))
     ) {
-      res.status(404).json({ error: " Category was not found!" });
+      throw createHttpError(404, "Category not found");
     }
 
     const updatedCategory = await prisma.category.update({
@@ -110,7 +111,7 @@ export const deleteCategory: RequestHandler = async (req, res, next) => {
     const validID = validateID(id);
 
     if (!validID) {
-      res.status(400).json({ error: "Invalid ID parameter!" });
+      throw createHttpError(400, "Invalid Category ID");
     }
 
     if (
@@ -120,7 +121,7 @@ export const deleteCategory: RequestHandler = async (req, res, next) => {
         },
       }))
     ) {
-      res.status(404).json({ error: " Category was not found!" });
+      throw createHttpError(404, "Category not found");
     } else {
       await prisma.category.delete({
         where: {
