5d4134ef1dc04c378f7dc3c8917eadb1a8480d30
Taha Zoabi <zoabi03@gmail.com>
2024-11-09 11:10:16 +0200

    page not found route and updated ts config

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,14 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/categories", categoriesRoutes);
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send({
     message: "Hello World!",
   });
+});
+
+app.use((_, res) => {
+  res.status(404).json({ error: "Page Not Found!" });
 });
 
 export default app;
